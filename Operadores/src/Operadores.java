6c304fe53b978829df72f14620a57dc64b18f634
Jhonael <jhonaelrocha@gmail.com>
2023-09-24 15:32:19 -0300

    Operadores

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        //Atribuição
+        String nome = "João";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean deficiente = false;
+
+        //Matemáticos
+        double soma = 10.5 + 15.7;
+        int subtração = 110 - 12;
+        int multiplicacao = 5 * 8;
+        double divisao = 10 / 9;
+        int modulo = 40 % 8;
+        double expressao = (1 + 4) * (38 - 2.55);
+
+        //Concatenação
+        String linguagem = "Linguagem" + "Java";
+        String concatenacao = "";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        concatenacao = 1 + "1" + 1 + "1";
+        concatenacao = "1" + 1 + 1 + 1;
+    }
+}
