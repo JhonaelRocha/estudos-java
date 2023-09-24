d90f772503d999e21e377a7254752f15da7dd95a
Jhonael <jhonaelrocha@gmail.com>
2023-09-24 15:03:20 -0300

    tipos de variaveis

--- Tipos e Variáveis/src/TiposEVariaveis.java
+++ Tipos e Variáveis/src/TiposEVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposEVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String nome = "Pedro";
+        double salario = 2000.44;
+
+        System.out.println("Nome : " + nome);
+        System.out.println("Salário : " + salario);
+
+
+        byte idade = 32;
+
+        System.out.println("Idade : " + idade);
+
     }
 }
