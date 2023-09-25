cc962053c9abf74fc8c3b16b6757ac2c08d381ca
Jhonael <jhonaelrocha@gmail.com>
2023-09-25 09:49:19 -0300

    Operadores

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -9,7 +9,7 @@ public class Operadores {
 
         //Matemáticos
         double soma = 10.5 + 15.7;
-        int subtração = 110 - 12;
+        int subtracao = 110 - 12;
         int multiplicacao = 5 * 8;
         double divisao = 10 / 9;
         int modulo = 40 % 8;
@@ -22,5 +22,28 @@ public class Operadores {
         concatenacao = 1 + 1 + 1 + "1";
         concatenacao = 1 + "1" + 1 + "1";
         concatenacao = "1" + 1 + 1 + 1;
+
+        //Ternário (Recebe retorno);
+        String verdade = (soma == divisao) ? "verdade" : "falso";
+        System.out.println(verdade);
+
+        //Comparativos
+        // == != > < >= <= 
+        if(linguagem == "LinguagemJava"){
+            System.out.println("É veradde.");
+        }
+        String nomeUm = "Pedro";
+        String nomeDois = new String("Pedro");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        //Lógicos
+        // && ||
+        if(modulo == 0 && subtracao == 98){
+            System.out.println("As duas são verdade");
+        }
+        if(multiplicacao == 40 || divisao == 1){
+            System.out.println("Uma delas é verdade");
+        }
     }
 }
